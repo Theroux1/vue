115470d5e26efc51ec5c15d57dfd801f2f0708f7
Theroux1 <gc.birchall@gmail.com>
2023-04-02 17:41:56 +0100

    28

---
+++ src/components/Modal.vue
@@ -0,0 +1,48 @@
+<script setup>
+
+defineProps({
+  show: Boolean
+});
+
+</script>
+
+<template>
+  <div class="modal-mask" v-if="show">
+    <div class="modal-container">
+
+<!--      <header>-->
+<!--        <slot name="header">-->
+<!--          default header-->
+<!--        </slot>-->
+<!--      </header>-->
+      <div>
+        <slot>default body</slot>
+      </div>
+      <footer class="border-t-2 pt-2">
+        <slot name="footer">
+          default footer
+        </slot>
+        <button class="block bg-green-400 rounded p-3 text-white" @click="$emit('close')">Close</button>
+      </footer>
+    </div>
+  </div>
+</template>
+
+<style>
+.modal-mask {
+  position: fixed;
+  inset: 0;
+  background: rgb(0 0 0 / 66%);
+
+  display: grid;
+  place-items: center;
+}
+
+.modal-container {
+  background: #fff;
+  padding: 1rem;
+  width: 80vw;
+  max-width: 500px;
+  border-radius: 12px;
+}
+</style>
--- src/views/TeamView.vue
+++ src/views/TeamView.vue
@@ -4,17 +4,38 @@ import TeamHeader from "@/components/Teams/TeamHeader.vue";
 import TeamMembers from "@/components/Teams/TeamMembers.vue";
 import TeamFooter from "@/components/Teams/TeamFooter.vue";
 import {useTeamStore} from "@/stores/TeamStore";
+import Modal from "@/components/Modal.vue";
+import {ref} from "vue";
 
 let team = useTeamStore();
 team.fill();
+
+let showModal = ref(false)
 </script>
 
 <template>
-  <TeamHeader  />
+  <TeamHeader @add="showModal = true"/>
 
   <div class="place-self-center flex flex-col gap-y-3" style="width: 725px">
-    <TeamMembers  />
+    <TeamMembers/>
   </div>
 
-  <TeamFooter  />
+  <TeamFooter/>
+
+  <Modal :show="showModal" @close="showModal = false">
+    <template #header>
+      <span></span>
+    </template>
+    <template #default>
+      <p>Add a new team member</p>
+      <form class="mt-5">
+        <div class="flex">
+
+          <input type="email" placeholder="Enter email address" class="flex-1">
+          <button type="submit">Add</button>
+        </div>
+
+      </form>
+    </template>
+  </Modal>
 </template>
